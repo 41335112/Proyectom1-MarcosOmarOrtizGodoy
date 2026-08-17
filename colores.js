39112cb5f0403ef3a40212d85ed1d4154a15f840
OmarOrtiz <omarortizgodoy@gmail.com>
2026-08-17 11:34:35 -0300

    funcion generar color hex

--- colores.js
+++ colores.js
@@ -9,3 +9,17 @@ const generador = document.lastElementChild(".gererador");
 funcion generarHex();
 funcion generarRgba();
 funcion generarPaleta();*/
+
+/* Genera el color Hex */
+function generarHex(){
+    const caracteres = "0123456789ABCDEF";
+
+    let color = "#";
+
+    for(let i=0; i < 6; i++){
+
+        const nroAleatorio = Math.floor(Math.random()* 16);
+        color += caracteres[nroAleatorio];
+    }
+    return color;
+}
